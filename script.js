ef344c35e56753c8b6adb410686e0f53002e34d2
Evelina Nyman <ina.nyman98@gmail.com>
2023-02-01 14:50:29 +0100

    klar för inlämning

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const getTodos = async () => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=7') //URL QUERY ?_limit=7 sätter en gräns på vår data så bara 7 todos laddas in
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        data.forEach(user => {
-            todos.push(user)
+        // console.log(data)
+        data.forEach(todo => {
+            todos.push(todo)
         })
         
         listTodos()
@@ -58,16 +58,18 @@ const createTodos = (todo) => {
         //Hämtar URL + id från alla todos, lägger sedan till en en funktion som ger möjlighet att toggla fram och tillbaka mellan klar och oklar
         fetch(BASE_URL + todo.id, {
             method: 'PATCH',
-            body:JSON.stringify({completed:!todo.completed}),
+            body:JSON.stringify({completed:!todo.completed}), //Vi hämtar det completed (t.ex true) som vårt element inte har (false)
             headers:{
-            'Content-type': 'application/json; charset=UTF-8',
+            'Content-type': 'application/json; charset=UTF-8', //Talar om för datorna att vi hämtar JSON
             },
         })
-        //lägger till klassen 'checked' som vi nu kan toggla fram och tillbaka emellan
+        //sedan när vi får tillbaka datan lägger vi till så att data och todo ska se likadana ut
+        
         .then(res => res.json())
         .then (data => {
             todo.completed = data.completed
-            todoItem.classList.toggle('checked')
+            //lägger till klassen 'checked' som vi nu kan toggla fram och tillbaka emellan klar och oklar när vi klickar på todoItem
+            todoItem.classList.toggle('checked') 
         }) 
      
     })
@@ -102,7 +104,17 @@ const createTodos = (todo) => {
             }
         })
     })
+    //Funktion som ger stängknappen på modalen- och kan ta bort den så den inte syns
+    closeBtn.addEventListener ('click', e => {
+   if(e.target == closeBtn) {
+    modal.classList.add('display-none')
+   }
+   if(e.target == window) {
+    modal.classList.add('display-none');
+   }
+})
 
+    //lägger till nya element efter förekommande element => t.ex todoList är förälder till todoItem 
     todoList.append(todoItem)
     todoItem.append(deleteBtn)
 
@@ -130,6 +142,7 @@ const handleSubmit = e =>{
     e.preventDefault()
     
 
+    //Skapar ny todo med det vi skriver in i inputen och lägger till completed: false som default
     const newTodo = {
         id: crypto.randomUUID(),
         title: document.querySelector('#todo-input').value,
@@ -140,7 +153,7 @@ const handleSubmit = e =>{
         // console.log('valideringsfel')
         return
     }
-
+    //Vi talar om att vi vill posta vår nya todo för databasen
     fetch(BASE_URL, {
         method:'POST',
         body:JSON.stringify(newTodo),
@@ -151,22 +164,13 @@ const handleSubmit = e =>{
     .then((response) => response.json())
     .then((data) => {
         const todoElement = createTodos(data)
-      todos.push(data)
+        todos.push(data)
+        console.log(todos)
         todoList.appendChild(todoElement);
         input.reset()
     });
 
 }
-
-//Funktion som ger stängknappen på modalen- och kan ta bort den så den inte syns
-closeBtn.addEventListener ('click', e => {
-   if(e.target == closeBtn) {
-    modal.classList.add('display-none')
-   }
-   if(e.target == window) {
-    modal.classList.add('display-none');
-   }
-})
 
 
 //Kör submit-funktion
